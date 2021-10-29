51e284d25f61e01ddd16ce8a18ceaff6f1fdc496
Renato Pereira Santos <renatodev95@gmail.com>
2021-10-29 08:18:40 -0300

    criação da classe empregado e configuração de DB

--- src/main/java/tech/getarrays/employeemanager/model/Employee.java
+++ src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -1,22 +1,28 @@
 package tech.getarrays.employeemanager.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 @Entity
 public class Employee implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private Long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String imageUrl;
+    @Column(nullable = false, updatable = false)
     private String employeeCode;
 
 }
